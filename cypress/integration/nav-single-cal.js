f1aa71aeb4dc5c249db116a6dad2b6b1bcb2d28a
jean <leejeanhwa@gmail.com>
2021-09-25 22:50:47 +0800

    20210925

--- cypress/integration/nav-single-cal.js
+++ cypress/integration/nav-single-cal.js
@@ -1,7 +1,12 @@
 /// <reference types="Cypress" />
 
 describe('Single Calendar Navigation', () => {
+    const url = 'https://www.iohotel.my/'
+    beforeEach('Visit URL', () => {
+        cy.visit(url);
+        cy.url().should('eq', url)
+        cy.title().should('contains', 'HOME | iO Hotel Kuala Lumpur')
+    })
     it('Test 1', () => {
-        cy.visit('https://www.google.com/');
     })
 })
