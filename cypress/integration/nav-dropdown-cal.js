40f072dc3b4d7ecadabbd09f488d7481df6b87b3
jean <leejeanhwa@gmail.com>
2022-02-16 19:07:02 +0800

    20220216

--- cypress/integration/nav-dropdown-cal.js
+++ cypress/integration/nav-dropdown-cal.js
@@ -32,6 +32,7 @@ context('Dropdown Calendar Navigation', () => {
         cy.get('.picker__day--highlighted').click()
         cy.get('@picker').click()
         cy.get('.picker__button--clear').click({force: true})
+        cy.wait(1000)
         cy.get('@picker').should('have.value', tmrDate)
     })
 
